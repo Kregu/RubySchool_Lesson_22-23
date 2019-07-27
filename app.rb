e9317e6794b09f606b4f766bceea22c291b8555a
Sergey Gorokhov <sergey.login@gmail.com>
2019-07-27 16:13:07 +0300

    Add About,  Visit, and Contacts menu to navbar

--- app.rb
+++ app.rb
@@ -24,9 +24,53 @@ get '/' do
   erb 'Can you handle a <a href="/secure/place">secret</a>?'
 end
 
+get '/about' do
+  erb :about
+end
+
+get '/visit' do
+  erb :visit
+end
+
+get '/contacts' do
+  erb :contacts
+end
+
+
 get '/login/form' do
   erb :login_form
 end
+
+post '/visit' do
+  @headresser = params[:headresser]
+  @client_name = params[:client_name]
+  @client_phone = params[:client_phone]
+  @date_time = params[:date_time]
+
+  # @title = "Thank you!"
+  # @message = "Dear #{@user_name}, we wait you at #{@date_time}"
+
+
+  f = File.open './public/users.txt', 'a'
+  f.write "headresser: #{@headresser}, client: #{@client_name}, phone: #{@client_phone}, date and time: #{@date_time}.\n"
+  f.close
+
+  where_user_came_from = session[:previous_url] || '/'
+  redirect to where_user_came_from
+end
+
+post '/contacts' do
+  @client_email = params[:client_email]
+  @client_message = params[:client_message]
+
+  f = File.open './public/contacts.txt', 'a'
+  f.write "client email: #{@client_email}\nmessage:\n#{@client_message}\n"
+  f.close
+
+  where_user_came_from = session[:previous_url] || '/'
+  redirect to where_user_came_from
+end
+
 
 post '/login/attempt' do
   session[:identity] = params['username']
